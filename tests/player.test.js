e1c8ef1b047d1d76c9295f81da9256f9f990432d
Victor Koech <vickoch20@gmail.com>
2024-07-17 13:41:11 +0300

    Fix: Update player factory and tests to handle both human and comp players

--- tests/player.test.js
+++ tests/player.test.js
@@ -2,16 +2,18 @@ import Player from '../src/player';
 import Gameboard from '../src/gameboard';
 
 describe('Player', () => {
-  let player;
+  let computerPlayer;
+  let humanPlayer;
   let enemyBoard;
 
   beforeEach(() => {
-    player = Player('human');  // Pass 'human' as an argument
+    computerPlayer = Player('computer');
+    humanPlayer = Player('human');
     enemyBoard = Gameboard();
   });
 
-  test('should make a legal move', () => {
-    const move = player.makeMove(enemyBoard);
+  test('computer should make a legal move', () => {
+    const move = computerPlayer.makeMove(enemyBoard);
     expect(move).toHaveProperty('x');
     expect(move).toHaveProperty('y');
     expect(move.x).toBeGreaterThanOrEqual(0);
@@ -20,10 +22,15 @@ describe('Player', () => {
     expect(move.y).toBeLessThan(10);
   });
 
-  test('should not repeat moves', () => {
+  test('human should return a dummy move', () => {
+    const move = humanPlayer.makeMove(enemyBoard);
+    expect(move).toEqual({ x: -1, y: -1 });
+  });
+
+  test('computer should not repeat moves', () => {
     const moves = new Set();
     for (let i = 0; i < 100; i++) {
-      const move = player.makeMove(enemyBoard);
+      const move = computerPlayer.makeMove(enemyBoard);
       const moveString = `${move.x},${move.y}`;
       expect(moves.has(moveString)).toBe(false);
       moves.add(moveString);
