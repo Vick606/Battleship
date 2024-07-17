d22650b80968e29c8da5518bf6aa642f3d5ff99f
Victor Koech <vickoch20@gmail.com>
2024-07-17 13:51:59 +0300

    Fix: Ensure comp player does not repeat moves

--- src/player.js
+++ src/player.js
@@ -1,11 +1,15 @@
 const Player = (type) => {
+    const previousMoves = new Set();
+  
     const makeMove = (enemyBoard) => {
       if (type === 'computer') {
         let x, y;
         do {
           x = Math.floor(Math.random() * 10);
           y = Math.floor(Math.random() * 10);
-        } while (enemyBoard.getBoard()[x][y] === 'hit' || enemyBoard.getBoard()[x][y] === 'miss');
+        } while (!enemyBoard.isValidMove(x, y) || previousMoves.has(`${x},${y}`));
+        
+        previousMoves.add(`${x},${y}`);
         return { x, y };
       } else {
         // For human players, return a dummy move
